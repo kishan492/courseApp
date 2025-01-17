e73d39618a05f6913ac03e3d287f5aafe9ee31d1
kishan492 <121510518+kishan492@users.noreply.github.com>
2025-01-17 19:57:10 +0530

    cors added to backend and setup frontend framework

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,18 @@ import express from "express"
 import dotenv from "dotenv"
 import connectdb from "./config/db.js";
 import courseRoute from "./routes/courseRouter.js"
+import cors from "cors"
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
 connectdb();
+
+const port = process.env.PORT || 3000;
 
 app.use('/api/course',courseRoute);
 app.get('/',(req,res)=>{
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <h1>Welcone to home page</h1>
+      </div>
+    
+  )
+}
+
+export default App
